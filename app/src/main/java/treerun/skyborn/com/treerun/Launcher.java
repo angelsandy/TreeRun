e0c683743e4451c1dedb211f1208a5519f7e48c8
Sandra Patricia Cerda Guerra <csandrapatricia@rocketmail.com>
2017-07-30 17:42:23 -0500

    Se modifica el launcher y se checa primera vez abierto

--- app/src/main/java/treerun/skyborn/com/treerun/Launcher.java
+++ app/src/main/java/treerun/skyborn/com/treerun/Launcher.java
@@ -1,7 +1,9 @@
 package treerun.skyborn.com.treerun;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.Window;
@@ -14,14 +16,21 @@ import java.util.TimerTask;
  */
 
 public class Launcher extends Activity {
-    private static final long SPLASH_SCREEN_DELAY = 3000;
-
+    private static long SPLASH_SCREEN_DELAY = 3000;
+    private final static String TREE_RUN_PREFERENCE = "TreeRunData";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.tr_launcher);
+        SharedPreferences treeRunData = getSharedPreferences(TREE_RUN_PREFERENCE, Context.MODE_PRIVATE);
+        if(treeRunData != null) {
+            if(treeRunData.getBoolean("isFirstTime",true)){
+                SPLASH_SCREEN_DELAY = 5000;
+                treeRunData.edit().putBoolean("isFirstTime",false).commit();
+            }
+        }
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
